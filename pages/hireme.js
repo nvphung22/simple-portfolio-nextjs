88c08f527659092aa09d84585b828cd95326d059
PhungNV <phungnv.bkdn@gmail.com>
2019-09-10 15:20:52 +0700

    push code

---
+++ pages/hireme.js
@@ -0,0 +1,12 @@
+import Layout from "../components/Layout";
+
+const HireMe = () => (
+  <Layout title="Hire Me">
+    <p>
+      You can hire me at{" "}
+      <a href="mailto:phungnv.bkdn@gmail.com">phungnv.bkdn@gmail.com</a>
+    </p>
+  </Layout>
+);
+
+export default HireMe;
